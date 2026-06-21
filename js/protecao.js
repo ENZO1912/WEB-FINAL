6df3785cdff5163c9195be067306e5638a0957b6
Enzo Rocha <enzor7039@gmail.com>
2026-06-21 14:53:48 -0300

    esqueci da proteção kkk

--- js/protecao.js
+++ js/protecao.js
@@ -1,64 +1,35 @@
-const usuarioLogado = JSON.parse(
-    localStorage.getItem("usuarioLogado")
-);
+const usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"));
 
+function protegerPagina() {
+  if (!usuarioLogado) {
+    alert("Faça login para acessar essa página!");
 
-
-function protegerPagina(){
-
-
-    if(!usuarioLogado){
-
-
-        alert("Faça login para acessar essa página!");
-
-
-        window.location.href="login.html";
-
-
-    }
-
-
+    window.location.href = "login.html";
+  }
 }
 
+function mostrarUsuario() {
+  if (usuarioLogado) {
+    const area = document.getElementById("usuario");
 
-
-function mostrarUsuario(){
-
-
-    if(usuarioLogado){
-
-
-        const area = document.getElementById("usuario");
-
-
-        if(area){
-
-            area.innerHTML = 
-            `
+    if (area) {
+      area.innerHTML = `
             Olá, ${usuarioLogado.nome}
             <button onclick="sair()">
             Sair
             </button>
             `;
-
-        }
-
-
     }
-
-
+  }
 }
 
+function sair() {
+  localStorage.removeItem("usuarioLogado");
 
-
-function sair(){
-
-
-    localStorage.removeItem("usuarioLogado");
-
-
-    window.location.href="login.html";
-
-
+  window.location.href = "login.html";
 }
+
+localStorage.setItem(
+    "usuarioLogado",
+    JSON.stringify(usuario)
+);
